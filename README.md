935a6b3112523113ed3235ff93289cdf59af7efa
Sagar Patel <sagar.patel@uci.edu>
2020-12-14 16:44:32 -0600

    change to project output format

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # MCTS-Sokoban
 A Sokoban solver using Monte Carlo Tree Search. 
-Install environment "pip install -e ." in this directory. Refer back to https://github.com/mpSchrader/gym-sokoban.
+
+To quickly execute the solver, run the commands "pip install -e ." and "python main.py --folder ./boards" 
+
+This version of the environment extends the popular gym environment for sokoban and adds a mcts_sokoban version of the environment to work with the project inputs and add a few functions for monte carlo tree search. Refer back to https://github.com/mpSchrader/gym-sokoban for the details on the original environment. 
+
+Execute the solver by calling main.py with the argument --file to attempt to solve the provided file path(es). To execute the solve on a folder, simply provide the folder path with the --folder argument. By default, the output is stored in "solve_log/[input file name].log" and the environment renders are not shown. To see a render of the gym environment, use "human" as the argument to --render_mode. To change the solve time limit set for each board, use the --time_limit argument.
--- main.py
+++ main.py
@@ -8,6 +8,13 @@ from pathlib import Path
 
 LEGAL_ACTIONS = [1,2,3,4]
 
+ACTION_MAP = {
+    'push up': "U",
+    'push down': "D",
+    'push left': "L",
+    'push right': "R",
+}
+
 def mcts_solve(args, file):
     if args.render_mode == "raw":
         observation_mode = "raw"
@@ -15,40 +22,38 @@ def mcts_solve(args, file):
         observation_mode = "tiny_rgb_array"
     else:
         observation_mode = "rgb_array"
-    log_dir = Path(args.log_dir)
-    log_dir.mkdir(exist_ok=True)
-    log = open(log_dir / "{}.log".format(file.stem), mode="w")
     dim_room, n_boxes, map = parse(filename= file)
+    actions = []
     env = gym.make("MCTS-Sokoban-v0", dim_room=dim_room, num_boxes=n_boxes, original_map=map, max_steps=args.max_steps)
     solver = MCTS(env=env, max_rollouts=args.max_rollouts, max_depth=args.max_depth, actions=LEGAL_ACTIONS)
     allocated_time = args.time_per_board * 60
     start_time = time()
-    i = 0
     while True:
         now = time()
         if now - start_time > allocated_time:
-            print("MCTS couldn't solve {} in allocated time.".format(file.name), file=log)
             break
         env.render(mode=args.render_mode)
         observation, reward, done, info = solver.take_best_action(observation_mode=observation_mode)
-        i += 1
-        print(info, file=log)
+        if "action.name" in info:            
+            actions.append(ACTION_MAP[info["action.name"]])
         if done and "mcts_giveup" in info:
             env.reset()
-            i = 0
+            actions.clear()
         elif done and info["all_boxes_on_target"]:
-            print("Solved {} in {} steps in {:.0f} minutes.".format(file.name, i, (now - start_time)/60), file=log)
             break
         elif done and info["maxsteps_used"]:
-            print("Resetting board.", file=log)
             env.reset()
-            i = 0
-        log.flush()
-    log.close()
+            actions.clear()
     env.render(mode=args.render_mode)
     sleep(3)
     env.close()
-
+    log_dir = Path(args.log_dir)
+    log_dir.mkdir(exist_ok=True)
+    with open(log_dir / "{}.log".format(file.stem), mode="w") as log:
+        print("{}".format(len(actions)), file=log, end="")
+        for action in actions:
+            print(" {}".format(action), file=log, end="")
+    
 def main(args):
     if args.file:
         for file in args.file:
@@ -62,11 +67,11 @@ if __name__ == '__main__':
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument("--file", nargs = "+", help= "file that defines the sokoban map")
     group.add_argument("--folder", help= "folder that contains files which define the sokoban map")
-    parser.add_argument("--render_mode", help="Obversation mode for the game", default="human")
+    parser.add_argument("--render_mode", help="Obversation mode for the game. Use human to see a render on the screen", default="raw")
     parser.add_argument("--max_rollouts", type=int, help="Number of rollouts per move", default=4000)
     parser.add_argument("--max_depth", type=int, help="Depth of each rollout", default=30)
     parser.add_argument("--max_steps", type=int, help="Max moves before game is lost", default=120)
-    parser.add_argument("--time_per_board", type=int, help="Allocated Time (in minutes) per board", default=60)
+    parser.add_argument("--time_limit", type=int, help="Allocated Time (in minutes) per board", default=60)
     parser.add_argument("--log_dir", type=str, help="Directory to log solve information", default="./solve_log")
     args = parser.parse_args()
     main(args)
